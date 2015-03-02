dd70c18c0ca4a3060df1e8db28c92b9acc2a753e
placeholder1423 <tcheng2012@hotmail.com>
2015-03-02 00:07:48 -0500

    change

---
+++ ConnectFourWithAI/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace ConnectFour
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbCanvas = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbCanvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbCanvas
+            // 
+            this.pbCanvas.BackColor = System.Drawing.Color.Yellow;
+            this.pbCanvas.Location = new System.Drawing.Point(12, 12);
+            this.pbCanvas.Name = "pbCanvas";
+            this.pbCanvas.Size = new System.Drawing.Size(630, 540);
+            this.pbCanvas.TabIndex = 0;
+            this.pbCanvas.TabStop = false;
+            this.pbCanvas.Paint += new System.Windows.Forms.PaintEventHandler(this.pbCanvas_Paint);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(158, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(342, 92);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 565);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pbCanvas);
+            this.Name = "Form1";
+            this.Text = "Connect4";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pbCanvas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbCanvas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ConnectFourWithAI/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConnectFour
+{
+    public partial class Form1 : Form
+    {
+
+        Dictionary<Keys,bool> input = new Dictionary<Keys, bool>();
+        int[,] slots;
+        int entryDisc;
+        int emptySlots;
+        bool isGameOver;
+        bool isPlayersTurn;
+
+        Brush bWhite = Brushes.White; 
+        Brush bBlack = Brushes.Black;
+        Brush bRed = Brushes.Red;     
+        Brush bGray = Brushes.Gray;
+
+        Random random;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            input.Add(Keys.Left, false);
+            input.Add(Keys.Right, false);
+            input.Add(Keys.Enter, false);
+
+            random = new Random();
+            isGameOver = false;
+            isPlayersTurn = true;
+            slots = new int[7, 6];
+            emptySlots = 42;
+            ResetGrid();
+
+            timer1.Interval = 50;
+            timer1.Tick += PlayerInput;
+            timer1.Start();
+        }
+
+        private void ResetGrid()
+        {
+            for (int i = 0; i < 7; i++)
+                for (int j = 0; j < 6; j++)
+                    slots[i, j] = 0;
+
+            entryDisc = 0;
+            emptySlots = 42;
+        }
+
+        private void PlayerInput(object sender, EventArgs e)
+        {
+            if (isGameOver && input[Keys.Enter])
+            {
+                label1.Visible = false;
+                ResetGrid();
+                isGameOver = false;
+            }
+            else
+            { 
+                if (isPlayersTurn)
+                {
+                    if (input[Keys.Left] && !input[Keys.Right] && entryDisc > 0 && findAndSwapEmptyLeftSlot(entryDisc, out entryDisc)) { }
+                    else if (input[Keys.Right] && !input[Keys.Left] && entryDisc < 6 && findAndSwapEmptyRightSlot(entryDisc, out entryDisc)) { }
+
+                        if (input[Keys.Enter])
+                        {
+                            DropDisc(1);
+                            PlaceNewEntryDisc();
+                            isPlayersTurn = false;
+                        }
+                }
+                else
+                {
+                    do
+                    {
+                        entryDisc = random.Next(0, 7);
+                    } while (slots[entryDisc, 0] != 0);
+
+                    DropDisc(-1);
+                    isPlayersTurn = true;
+                    if (emptySlots > 0)
+                    {
+                        PlaceNewEntryDisc();
+                    }
+                }
+                checkForGameOver();
+            }
+            pbCanvas.Invalidate();
+        }
+
+        private void checkForGameOver()
+        {
+            if (emptySlots==0)
+            {
+                EndGame("No Winners");
+            }
+            else 
+            {
+                for (int x = 0; x < 7; x++)
+                {
+                    for (int y = 0; y < 6; y++)
+                    {
+                        if (x <= 3)
+                        {
+                            int sumHorizontal = 0;
+                            for (int i = x; i < x+4; i++)
+                                sumHorizontal += slots[i, y];
+                            checkScore(sumHorizontal);
+                        }
+                        if (y <=2) 
+                        {
+                            int sumVertical = 0;
+                            for (int i = y; i < y + 4; i++)
+                                sumVertical += slots[x, i];
+                            checkScore(sumVertical);	 
+                        }
+                        if (x <= 3 && y <=2) 
+                        {
+                            int sumDiag1 = 0;
+                            for (int i = x, j = y; i < x + 4; i++, j++)
+                                sumDiag1 += slots[i, j];
+                            checkScore(sumDiag1);
+                        }
+                        if (x >= 3 && y >= 3)
+                        {
+                            int sumDiag2 = 0;
+                            for (int i = x, j = y; j > y - 4; i--, j--)
+                                sumDiag2 += slots[i, j];
+                            checkScore(sumDiag2);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void checkScore(int x)
+        {
+            if (x == 4)
+                EndGame("You Won!");
+            if (x == -4)
+                EndGame("Computer Wins!");
+        }
+
+        private void EndGame(string message)
+        {
+            isGameOver = true;
+            label1.Visible = true;
+            label1.Text = "Game over - "+ message + "\nPress Enter to restart";
+            isPlayersTurn = true;
+        }
+
+        private void PlaceNewEntryDisc()
+        {
+            entryDisc = 0;
+
+            while (slots[entryDisc, 0] != 0)
+                entryDisc++;
+        }
+
+        private void DropDisc(int playerValue)
+        {
+            emptySlots--;
+            int index = 5;
+            while (slots[entryDisc, index] != 0)
+            {
+                index--;
+            }
+            slots[entryDisc, index] = playerValue;
+        }
+
+        private bool findAndSwapEmptyLeftSlot(int input, out int output)
+        {
+            int i;
+            for (i = input -1; i >= 0; i--)
+            {
+                if (slots[i, 0] == 0)
+                {
+                    output = i;
+                    return true;
+                }
+            }
+            output = input;
+            return false;
+        }
+
+        private bool findAndSwapEmptyRightSlot(int input, out int output)
+        {
+            int i;
+            for (i = input + 1; i < 7; i++)
+            {
+                if (slots[i, 0] == 0)
+                {
+                    output = i;
+                    return true;
+                }
+            }
+            output = input;
+            return false;
+        }
+
+        private void pbCanvas_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            for (int i = 0; i < 7; i++)
+                for (int j = 0; j < 6; j++)
+                {
+                    if (slots[i, j] == 0) { g.FillEllipse(bWhite, new Rectangle(i * 90, j * 90, 90, 90)); }
+                    else if (slots[i, j] == 1) { g.FillEllipse(bRed, new Rectangle(i * 90, j * 90, 90, 90)); }
+                    else if (slots[i, j] == -1) { g.FillEllipse(bBlack, new Rectangle(i * 90, j * 90, 90, 90)); }
+                    else if (slots[i, j] == 2) { g.FillEllipse(bGray, new Rectangle(i * 90, j * 90, 90, 90)); }
+
+                    g.FillEllipse(bGray, new Rectangle(entryDisc * 90, 0, 90, 90));
+                }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            input[e.KeyCode] = true;
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            input[e.KeyCode] = false;
+        }
+
+    }
+}
